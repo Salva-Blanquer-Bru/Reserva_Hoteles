9c8e309960509ae9f6fd27670109364610aa5919
salva <salvablablanquer0@gmail.com>
2021-02-11 22:56:34 +0100

    Mejoras añadidas

--- src/MainG.java
+++ src/MainG.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainG extends JFrame {
+    public JButton GenContraseña;
+    public JLabel contraseña;
 
     public MainG() {
-        super("Login");
+        super("Generador De Contraseñas");
 
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -13,36 +20,73 @@ public class MainG extends JFrame {
 
         JPanel Menu = new JPanel();
         Menu.setLayout(new GridLayout(3, 1, 5, 5));
+        Menu.add(new JLabel("Contraseña:"));
+        new JLabel("Contraseña:");
 
 
-        JPanel VerContraseña = new JPanel();
-        VerContraseña.add(new JLabel("Contraseña:"));
-        VerContraseña.add(new JLabel(), BorderLayout.CENTER);
+        contraseña = (new JLabel(""));
+        // GenContraseña.setLayout(new FlowLayout());
+        GenContraseña = new JButton("Generar contraseña");
+        GenContraseña.addActionListener(new ListenerButton());
+        // GenContraseña.setSize(10,10);
 
-        JPanel GenContraseña = new JPanel();
-        GenContraseña.add(new JButton("Generar contraseña"), BorderLayout.WEST);
 
         JPanel opciones = new JPanel();
+        opciones.setLayout(new GridLayout(4, 1, 5, 5));
         opciones.add(new Checkbox("MAYUSCULAS"));
         opciones.add(new Checkbox("minusculas"));
         opciones.add(new Checkbox("Numeros"));
         opciones.add(new Checkbox("Simbolos"));
 
+        JPanel LongitudContraseña = new JPanel();
+        JProgressBar progressBar = new JProgressBar();
+
+        JSlider sliderContra;
+        LongitudContraseña.add(sliderContra = new JSlider(JSlider.HORIZONTAL, 0, 20, 5), BorderLayout.PAGE_START);
+        JTextField NumContraseña;
+        sliderContra.setBorder(new TitledBorder("Deslizame"));
+        progressBar.setModel(sliderContra.getModel());
+        LongitudContraseña.add(NumContraseña = new JTextField("25"), BorderLayout.SOUTH);
+        sliderContra.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int NumberCharac = sliderContra.getValue();
+                NumContraseña.setText(Integer.toString(NumberCharac));
+            }
+        });
 
         add(Menu);
 
-        Menu.add(VerContraseña);
+        Menu.add(contraseña);
+        Menu.add(GenContraseña);
+
 
         Menu.add(opciones);
+//  Menu.add(GenContraseña);
+        Menu.add(LongitudContraseña);
+        JPanel pBar = new JPanel();
+        pBar.setLayout(new FlowLayout());
+        pBar.add(progressBar);
+        progressBar.setSize(10, 10);
+        Menu.add(pBar);
 
-        Menu.add(GenContraseña);
-        setSize(400, 400);
+        setSize(600, 400);
         setVisible(true);
     }
 
     public static void main(String[] args) {
         new MainG();
-
     }
+
+    private class ListenerButton implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            contraseña.setText("prueba");
+        }
+    }
+
+
 }
 
